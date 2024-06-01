73720d4d393d4871d3efaf4000dcb1ae7c24f012
halith-smh <mohamedhalith.smh@gmail.com>
2024-06-01 11:40:30 +0530

    Fixed Firebase Connection

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,6 +9,8 @@ export const API_OPTIONS = {
   },
 };
 
+export const FIREBASE_API = process.env.FIREBASE_API;
+export const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
 export const GUEST_ID = {email: "guest@netflix.com", password: "Password@1234"};
 
 
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { FIREBASE_API, FIREBASE_APP_ID } from "./constants";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA3D-s2UExK2u1ZpgqGRTz9lx-pH-UlyJ8",
+  apiKey: FIREBASE_API,
   authDomain: "netflixgpt-by-smh.firebaseapp.com",
   projectId: "netflixgpt-by-smh",
   storageBucket: "netflixgpt-by-smh.appspot.com",
   messagingSenderId: "59383730481",
-  appId: "1:59383730481:web:00c9a2a82f4935b4f56010"
+  appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase
