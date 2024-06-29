21cadf9a5d88891b83dfb46049ed7c2a63533643
halith-smh <mohamedhalith.smh@gmail.com>
2024-06-29 22:00:48 +0530

    GPTSearch: Poster Not Found Update

--- src/components/SquareCard.jsx
+++ src/components/SquareCard.jsx
@@ -4,7 +4,7 @@ import { IMAGE_URI } from "../utils/constants";
 const SquareCard = ({ movie, gptPage }) => {
   return (
     <div className="w-[8rem] h-[14rem]  md:w-[14rem] md:h-[24rem]">
-      {gptPage && !movie.Poster ? 'No Poster' : ''}
+      {gptPage && !movie.Poster ? 'Poster Not Found' : ''}
       <img
         className="w-full h-full rounded-md hover:opacity-80"
         src={gptPage ? movie?.Poster : IMAGE_URI + movie.poster_path}
